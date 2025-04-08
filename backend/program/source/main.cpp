b793e9891b3fb7ca4c8629d1fbc80d3627c2dbbb
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-04-08 19:39:47 +0100

    Made frontend and backend interact with each other

--- backend/program/source/main.cpp
+++ backend/program/source/main.cpp
@@ -1,16 +1,27 @@
-#include <iostream>
+#include <crow/app.h>
+#include <crow/common.h>
+#include <crow/http_response.h>
+#include <crow/json.h>
+#include <crow/middlewares/cors.h>
 
 #include "main.hpp"
-#include <crow/app.h>
 
 int main()
 {
-  crow::SimpleApp app;
+  crow::App<crow::CORSHandler> app;
+  auto cors = app.get_middleware<crow::CORSHandler>();
 
-  CROW_ROUTE(app, "/")([]() { return "Hello, World!"; });
+  CROW_ROUTE(app, "/message")
+    .methods(crow::HTTPMethod::GET)(
+      []()
+      {
+        crow::json::wvalue response({
+          {"message", "Hello, world!"},
+          {"number", 42},
+        });
+        return response;
+      });
 
   app.port(8080).multithreaded().run();
   return 0;
 }
-
-void say_hello() { std::cout << "Hello, World!" << std::endl; }
